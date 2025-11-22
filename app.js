9dd3cea525a7e0eb82b42b1ab7df5448083ab424
Senpai <clevercoder0307@gmail.com>
2025-11-23 00:11:22 +0530

    express setup and routes done

---
+++ app.js
@@ -0,0 +1,22 @@
+import express from "express"
+import { PORT } from "./config/env.js"
+
+import userRouter from "./routes/user.route.js"
+import authRouter from "./routes/auth.route.js"
+import subscriptionRouter from "./routes/subscription.route.js"
+
+const app = express()
+
+app.use("/api/v1/auth", authRouter)
+app.use("/api/v1/users", userRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter)
+
+app.get("/", (req, res) => {
+    res.send("Heyy hello hii")
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Subscription API is running on http://localhost:${PORT}`)
+})
+
+export default app
