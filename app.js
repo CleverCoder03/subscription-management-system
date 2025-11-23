747bd6a869deab29529e30d328aef3f3763b9da7
Senpai <clevercoder0307@gmail.com>
2025-11-23 23:33:10 +0530

    connected successfully to mongoDB database

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
-import express from "express"
-import { PORT } from "./config/env.js"
+import express from "express";
+import { PORT } from "./config/env.js";
 
-import userRouter from "./routes/user.route.js"
-import authRouter from "./routes/auth.route.js"
-import subscriptionRouter from "./routes/subscription.route.js"
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+import subscriptionRouter from "./routes/subscription.route.js";
+import connectToDatabase from "./database/mongodb.js";
 
-const app = express()
+const app = express();
 
-app.use("/api/v1/auth", authRouter)
-app.use("/api/v1/users", userRouter)
-app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
 
 app.get("/", (req, res) => {
-    res.send("Heyy hello hii")
-})
+  res.send("Heyy hello hii");
+});
 
-app.listen(PORT, ()=>{
-    console.log(`Subscription API is running on http://localhost:${PORT}`)
-})
+app.listen(PORT, async () => {
+  console.log(`Subscription API is running on http://localhost:${PORT}`);
 
-export default app
+  await connectToDatabase();
+});
+
+export default app;
